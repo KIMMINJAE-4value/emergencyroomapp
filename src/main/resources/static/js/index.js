5cf3927c0b7ec94dabc58b467856449dd1151ad0
KIMMINJAE-4value <yt90as@4valuesoft.com>
2024-10-24 16:23:12 +0900

    네이버 맵 검색 구현

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,17 @@
-console.log('test')
+let search = document.querySelector(".search-query");
+
+// fetch('', {})
+
+search.oninput = (word) => {
+    if (word.target.value !== '') {
+        naver.maps.Service.geocode(
+            {
+                query: word.target.value,
+            },
+            (status, response) => {
+                let result = response.v2
+                console.log(result)
+            },
+        )
+    }
+}
