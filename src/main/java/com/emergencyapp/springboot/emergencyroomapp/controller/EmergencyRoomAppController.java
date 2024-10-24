fdee98e424ee1f57b9884e9c1874de5fdabdedb9
KIMMINJAE-4value <yt90as@4valuesoft.com>
2024-10-24 14:45:43 +0900

    spring 타임리프 값 넘기기

--- src/main/java/com/emergencyapp/springboot/emergencyroomapp/controller/EmergencyRoomAppController.java
+++ src/main/java/com/emergencyapp/springboot/emergencyroomapp/controller/EmergencyRoomAppController.java
@@ -1,12 +1,15 @@
 package com.emergencyapp.springboot.emergencyroomapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EmergencyRoomAppController {
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("name", "dd");
+
         return "thymeleaf/index";
     }
 }
