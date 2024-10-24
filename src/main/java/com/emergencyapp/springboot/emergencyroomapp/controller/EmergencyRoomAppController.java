a19508c81a059047c1c3394ab9c9170d935b17cf
KIMMINJAE-4value <yt90as@4valuesoft.com>
2024-10-24 13:59:15 +0900

    스크립트 및 index.html 가져오기

--- src/main/java/com/emergencyapp/springboot/emergencyroomapp/controller/EmergencyRoomAppController.java
+++ src/main/java/com/emergencyapp/springboot/emergencyroomapp/controller/EmergencyRoomAppController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class EmergencyRoomAppController {
     @RequestMapping("/")
-    @ResponseBody
-    public String responseViewV2() {
-        return "index";
+    public String home() {
+        return "thymeleaf/index";
     }
 }
