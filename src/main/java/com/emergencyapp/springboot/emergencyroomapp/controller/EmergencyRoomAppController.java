856c61ac27729f8241ca9f001536705001d9da8f
KIMMINJAE-4value <yt90as@4valuesoft.com>
2024-10-25 15:08:55 +0900

    불필요 모델 제거

--- src/main/java/com/emergencyapp/springboot/emergencyroomapp/controller/EmergencyRoomAppController.java
+++ src/main/java/com/emergencyapp/springboot/emergencyroomapp/controller/EmergencyRoomAppController.java
@@ -1,15 +1,12 @@
 package com.emergencyapp.springboot.emergencyroomapp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EmergencyRoomAppController {
     @RequestMapping("/")
-    public String home(Model model) {
-        model.addAttribute("name", "dd");
-
+    public String home() {
         return "thymeleaf/index";
     }
 }
